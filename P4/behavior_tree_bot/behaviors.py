78a36c162def5b4a64e901e2a613f710d4521cb3
jboy5544123 <jocobrow@ucsc.edu>
2020-10-28 21:05:04 -0700

    Initial start

--- P4/behavior_tree_bot/behaviors.py
+++ P4/behavior_tree_bot/behaviors.py
@@ -3,7 +3,7 @@ sys.path.insert(0, '../')
 from planet_wars import issue_order
 
 
-def attack_weakest_enemy_planet(state):
+def attack_weakest_enemy_planet(state): 
     # (1) If we currently have a fleet in flight, abort plan.
     if len(state.my_fleets()) >= 1:
         return False
@@ -39,3 +39,39 @@ def spread_to_weakest_neutral_planet(state):
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+
+
+#Attacking Strategy:
+
+    #Cheese - attacking an enemy planet that just sent a fleet to another planet
+
+    #Suppression - continually send ships at the rate they are being made at a certain enemy planet after
+
+    #Charge - Check to see if at a significantly higher fleet count than an opposing planet if so fire if not continue charging
+
+    #
+
+#Spreading Strategy:
+
+    #taking a neutral planet thats closer to us right after it is taken by opponent
+
+    #taking a neutral planet that is reasonably close to us in order of least cost while maintaining out original fleet count
+    #to a reasonable degree
+
+    #
+
+
+#Defending Strategy:
+
+    #only defend if you have the resources to defend
+
+    #if the opponent is sending a fleet to take one of our planets and we have time to rienforce it, rienforce the planet
+
+    #if one of our planets needs defence defend it
+
+    #if we are about to take a planet defend it
+
+    #when a planet is about to get taken from us. if we are winning then dont abandon the planet,
+    #if we are losing abandon the planet unless its our last planet
+
+    #
--- P4/behavior_tree_bot/checks.py
+++ P4/behavior_tree_bot/checks.py
@@ -9,3 +9,26 @@ def have_largest_fleet(state):
              + sum(fleet.num_ships for fleet in state.my_fleets()) \
            > sum(planet.num_ships for planet in state.enemy_planets()) \
              + sum(fleet.num_ships for fleet in state.enemy_fleets())
+
+
+
+
+#if theres an enemy fleet in transit towards a neutral planet that is closer to us
+
+#if we are about to lose a planet
+
+#if we have a planet that is significantly larger than an opposing planet that is currently being suppressed
+
+#if we are close enough to capture an opposing planet that just sent out a fleet before they regrow their forces
+
+#if we are winning or losing
+
+#check who is the weakest player in 3+ player games
+
+#if we are about to take a planet and it will need defense
+
+#check distance between all planets
+
+#comparing fleet sizes of planets
+
+#determine what is a safe minimum fleet size for planets that are supressing other planets
